0817493c8df3cf1e22396e5a209b136de983ab9f
Ivan <ivannsharipkov@gmail.com>
2021-03-21 01:14:02 +0300

    Scene generation(just bg)

---
+++ 2D_training/Assets/scripts/Camera_script.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_script : MonoBehaviour
+{
+    Background_generation generator;
+    void Start()
+    {
+        generator = GetComponent<Background_generation>();
+    }
+
+    void Update()
+    {
+        
+    }
+    /// <summary>
+    /// Cleans level behind screen
+    /// </summary>
+    /// <param name="cleaner"></param>
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        generator.ReplaceCloud(collision.gameObject);
+    }
+}
